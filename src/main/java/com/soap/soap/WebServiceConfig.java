e4f7e79d1b5b3ebd61acaa5f8adba619cdb586ae
COdiwuor <cyrilondime@gmail.com>
2019-10-30 13:06:08 +0300

    Latest Update

--- src/main/java/com/soap/soap/WebServiceConfig.java
+++ src/main/java/com/soap/soap/WebServiceConfig.java
@@ -1,5 +1,4 @@
 package com.soap.soap;
-
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
